22276d781434e876376cbad0ca557f20c4cd8d8d
Abdul Kareem <41697424+Salmanabdul786@users.noreply.github.com>
2024-08-27 23:57:10 +0530

    Deployment

--- Backend/server.js
+++ Backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from 'path';
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import userRoute from "./routes/user.routes.js";
@@ -13,11 +14,21 @@ const PORT = process.env.PORT || 5000;
 // app.get("/",(req, res)=>{
 //     res.send("Hello World");
 // })
+
+const __dirname=path.resolve();
+
 app.use(express.json()); // parse from req.body
 app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoute);
+
+
+
+app.use(express.static(path.join(__dirname,"/Frontend/dist")));
+app.get("*",(req,res)=>{
+res.sendFile(path.join(__dirname,"Frontend","dist","index.html"));
+});
 
 server.listen(PORT, () => {
   connectTodb();
