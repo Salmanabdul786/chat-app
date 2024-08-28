d57ecb78c17ae80692993c49831b355d2fdfa9c0
Abdul Kareem <41697424+Salmanabdul786@users.noreply.github.com>
2024-08-28 23:14:59 +0530

    Deployment

--- Backend/utils/generateToken.js
+++ Backend/utils/generateToken.js
@@ -7,7 +7,7 @@ const generateTokenandSetcokkie=(userId,res)=>{
         maxAge:15*24*60*60*1000,
         httpOnly:true,
         sameSite:"strict",
-        secure:process.env.NODE_ENV !== "production",
+        secure:process.env.NODE_ENV !== "development",
     });
 }
 
